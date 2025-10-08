cc697bce4669573bf98ff4a0188993467bffdc85
polsaniprasad <polsaniprasad@gmail.com>
2025-10-07 21:17:16 -0500

    Updated Code

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,21 @@
+
 const config = {
   testDir: './tests',
-  use: { headless: false },
+  use: { headless: false ,
+    screenshot : "on",
+  video : "on"
+  },
+  
   projects: [
     { name: 'Google Chrome', use: { channel: 'chrome' } },
-    { name: 'firefox', use: { browserName: 'firefox' } },
-    { name: 'webkit', use: { browserName: 'webkit' } }, // Safari engine
+   { name: 'firefox', use: { browserName: 'firefox' } },
+   { name: 'webkit', use: { browserName: 'webkit' } }, // Safari engine
   ],
+  reporter : [ ["dot"], ["json", {
+     outputFile:"jsonReports/jsonReports.json" 
+    } ], ["html", { 
+      open: "never"
+    }] ]
 };
 
 module.exports = config;
